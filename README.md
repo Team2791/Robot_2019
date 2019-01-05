4ceaf2f1f7451d695320dbd3ef222f3a7d2c847c
Max Sichel <msik314@gmail.com>
2019-01-05 14:18:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+2019 Robot Code
+===============
+This repository contains FRC Team 2791's robot code for the 2019 Competition season.
---
+++ src/main/java/frc/robot/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Do NOT modify this file unless you know what you are doing.
+ */
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String... args) {
+        RobotBase.startRobot(Robot::new);
+    }
+}
