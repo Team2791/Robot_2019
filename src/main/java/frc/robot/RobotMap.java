d97ab82d5e139a70efaa548ed28e3b33a8e17029
Max Sichel <msik314@gmail.com>
2019-01-26 10:08:05 -0500

    Update robot map

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,8 +3,15 @@ package frc.robot;
 public class RobotMap {
     //The numbers in this map are temporary values until we get an actual robot to use.
     //Drivetrain
-    public static final int kLeftTalon = 27;
-    public static final int kRightTalon = 20;
-    public static final int[] kLeftVictors = {32, 36};
-    public static final int[] kRightVictors = {35, 37};
+    public static final int kLeftTalon = 20;
+    public static final int kRightTalon = 21;
+    public static final int[] kLeftVictors = {30, 32};
+    public static final int[] kRightVictors = {31, 33};
+
+    public static final int kFrontLiftTalon = 22;
+    public static final int kBackLiftTalon = 23;
+    public static final int kRollerVictor = 34;
+
+    public static final int kElevatorTalon = 24;
+    public static final int kIntakeVictor = 35;
 }
